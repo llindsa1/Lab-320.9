2c7d1fbb44ee7706fc53a3f5149c08dd9b0941f6
llindsa1 <lindsayuchiha@icloud.com>
2024-11-13 10:14:20 -0500

    4

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -13,4 +13,14 @@ function App() {
       setTodos([...todos, { text: formData, completed: false }]);
     }
   };
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+  const toggleComplete = (index) => {
+    setTodos(
+      todos.map((todo, i) =>
+      i === index ? {...todo, completed: !todo.completed } : todo
+    )
+    );
+  };
   
